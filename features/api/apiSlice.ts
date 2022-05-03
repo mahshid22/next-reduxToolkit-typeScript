3e780f61e5b0a157fccbfa1bce9eeb72c73eb116
mahshid22 <mahshid.pouyafar@gmail.com>
2022-05-03 23:51:50 +0430

    remove proxy

--- features/api/apiSlice.ts
+++ features/api/apiSlice.ts
@@ -2,8 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl:
-      "https://cors-anywhere.herokuapp.com/http://fakeapi.jsonparseronline.com/",
+    baseUrl: "http://fakeapi.jsonparseronline.com/",
     prepareHeaders: (headers) => {
       headers.set("Access-Control-Allow-Credentials", "true");
       return headers;
