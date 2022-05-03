7729322d6cf16d73a4135421be078b90fe179ed6
mahshid22 <mahshid.pouyafar@gmail.com>
2022-05-03 21:57:43 +0430

    add cors policy

--- features/api/apiSlice.ts
+++ features/api/apiSlice.ts
@@ -2,7 +2,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://fakeapi.jsonparseronline.com/",
+    baseUrl:
+      "https://cors-anywhere.herokuapp.com/http://fakeapi.jsonparseronline.com/",
     prepareHeaders: (headers) => {
       headers.set("Access-Control-Allow-Credentials", "true");
       return headers;
